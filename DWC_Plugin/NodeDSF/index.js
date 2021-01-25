995e93129d8bc2ba97d64dfc9291911290cfee4b
Minty Trebor <mintytrebor@gmail.com>
2021-01-25 12:35:44 +0000

    Update index.js

--- DWC_Plugin/NodeDSF/index.js
+++ DWC_Plugin/NodeDSF/index.js
@@ -4,13 +4,11 @@ import { registerRoute } from '../../routes'
 
 import NodeDSF from './NodeDSF.vue'
 
-//<iframe class="NodeIFrame" src="http://localhost:1880/NodeDSF" scrolling="no" frameborder="0" allowtransparency="true" width="100%" height=auto></iframe>
-
 // Register a route via Settings -> Object Model
 registerRoute(NodeDSF, {
 	Settings: {
 		ObjectModel: {
-			icon: 'mdi-file-tree',
+			icon: 'mdi-resistor-nodes',
 			caption: 'NodeDSF',
 			path: '/NodeDSF'
 		}
